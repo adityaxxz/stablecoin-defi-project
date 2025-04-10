dfb9e777374488279da257e9c1acc6cb99c4c387
adityaxxz <adityaranjan5995@gmail.com>
2025-04-11 00:20:54 +0530

    fixes

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -13,18 +13,18 @@ import { DecentralizedStableCoin } from "../src/DecentralizedStableCoin.sol";
  *
  * The system is designed to be as minimal as possible, and have the tokens maintain a 1 token == $1 peg at all times.
  * This is a stablecoin with the properties:
- * - Exogenously Collateralized
+ * - Exogenously Collateralized (like DAI, because its backed by ETH or USDT, which have independent use cases.)
  * - Dollar Pegged
  * - Algorithmically Stable
  *
  * It is similar to DAI if DAI had no governance, no fees, and was backed by only WETH and WBTC.
  *
- * Our DSC system should always be "overcollateralized". At no point, should the value of
- * all collateral < the $ backed value of all the DSC.
+ * Our DSC system should always be "overcollateralized". 
+ * At no point, should the value of all collateral < the $ backed value of all the DSC.
  *
  * @notice This contract is the core of the Decentralized Stablecoin system. It handles all the logic
  * for minting and redeeming DSC, as well as depositing and withdrawing collateral.
- * @notice This contract is based on the MakerDAO DSS system
+ * @notice This contract is based on the MakerDAO DSS(Dai StableCoin System) system.
  */
 contract DSCEngine is ReentrancyGuard {
     error DSCEngine__NeedsMoreThanZero();
@@ -191,11 +191,6 @@ contract DSCEngine is ReentrancyGuard {
         uint256 totalCollateralToRedeem = tokenAmountFromDebtCovered + bonusCollateral;
     }
 
-    function getHealthFactor() external view {
-        
-    }
-
-
     /*////////////////////////////////////////////////////////////////////////*/
     /*                                 INTERNAL & PUBLIC                      */
     /*////////////////////////////////////////////////////////////////////////*/
@@ -256,13 +251,7 @@ contract DSCEngine is ReentrancyGuard {
         }
     }
 
-    function _calculateHealthFactor(
-        uint256 totalDscMinted,
-        uint256 collateralValueInUsd
-    )
-        internal
-        pure
-        returns (uint256)
+    function _calculateHealthFactor(uint256 totalDscMinted, uint256 collateralValueInUsd) internal pure returns (uint256)
     {
         if (totalDscMinted == 0) return type(uint256).max;
         uint256 collateralAdjustedForThreshold = (collateralValueInUsd * LIQUIDATION_THRESHOLD) / LIQUIDATION_PRECISION;
